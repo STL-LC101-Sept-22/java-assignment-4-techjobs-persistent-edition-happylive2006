642984b525d4f5fbb820fb6818519c99045d5499
Meiling <happylive_2006@hotmail.com>
2023-03-17 23:35:02 -0500

    update

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.stream.Location;
+
 public class Employer extends AbstractEntity {
+    @NotBlank
+    @Size(max=100)
+    @NotNull
+    public String location;
+
+
 
 
 }
